2fb99be7b5709c0c180edff2dd99673ec3c41c8e
disconaked <nolan.klinke@gmail.com>
2018-05-30 21:34:43 -0400

    inital js

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+
+var wins = 0;
+var guessesRemaining = 6;
+
+var country = ["Germany", "Brazil", "France", "Argentina", "Spain", "Chile", "Belgium", "Netherlands", "Portugal", "England"];
+var countryRandom = country[Math.floor(Math.random()*country.length)];
+console.log(countryRandom);
+
+
+
+
+
+
