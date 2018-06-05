c6ce68d112298e2f3992659567357d067222b4e1
disconaked <nolan.klinke@gmail.com>
2018-06-04 21:44:27 -0400

    eon

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,15 +4,6 @@ var guessesRemaining = 6;
 var computerChoice = "";
 var country = ["Germany", "Brazil", "France", "Argentina", "Spain", "Chile", "Belgium", "Netherlands", "Portugal", "England"];
 
-
-function refreshLetter() {
-    computerChoice = country[Math.floor(Math.random() * country.length)];
-    console.log(computerChoice);
-
-}
-
-refreshLetter();
-
 //create buttons for user
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
@@ -26,11 +17,29 @@ for (var i = 0; i < letters.length; i++) {
     
 }
 
-for (var j = 0; j < computerChoice.length; j++) {
-    computerChoice[j] = "_";
-    document.getElementById("currentWord").innerHTML += " " + computerChoice[j];
+//generate random word to guess
+function refreshWord() {
+    computerChoice = country[Math.floor(Math.random() * country.length)];
+    console.log(computerChoice);
 
 }
+
+refreshWord();
+
+//seperate computerChoice into individual characters
+for (var j = 0; j < computerChoice.length; j++) {
+
+    //document.getElementById("currentWord").innerHTML += " " + computerChoice[j];
+    var newSpan = $("<span>");
+    newSpan.text(" " + computerChoice[j]).after(" ");
+    newSpan.addClass("border-bottom m-3");
+    newSpan.appendTo("#currentWord");
+}
+
+
+
+
+
 
 
 
